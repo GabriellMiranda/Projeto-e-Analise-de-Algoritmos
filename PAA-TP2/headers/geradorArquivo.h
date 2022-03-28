c9b7e1a61aa26f1d905fcc91754b175a98b8e776
Gabriel Miranda <70436348+GabriellMiranda@users.noreply.github.com>
2022-03-28 16:04:48 -0300

    Delete PAA-TP2 directory

--- PAA-TP2/headers/geradorArquivo.h
+++
@@ -1,8 +0,0 @@
-#ifndef GERADORARQUIVO_H
-#define GERADORARQUIVO_H
-
-void gerarCaracteres(char* str);
-
-void gerarArquivo(char* nomeArquivo,int tamLinha, int tamColuna, int tempoSamus,int tempoLava);
-
-#endif
--- PAA-TP2/headers/modoAnalise.h
+++
@@ -1,7 +0,0 @@
-#ifndef MODOANALISE_H
-#define MODOANALISE_H
-
-void gerarArquivos();
-void calcularTempos();
-
-#endif
